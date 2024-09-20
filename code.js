9f38a80cbb125abd1bacb76b2ec1ac2a11541b0b
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-09-20 12:21:13 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
-function fib(n) {
-    return n;
+function fib(n) 
+{
+    if (n==0) return [0];
+    if (n==1) return [0,1];
+    
+    let arr = [0,1];
+    
+    for (let i = 2; i <= n; i++)
+    {
+        arr.push(arr[i-1] + arr[i-2]);
+    }
+    return arr;
 }
