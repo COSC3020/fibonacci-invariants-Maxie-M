c4d42e560b4db70441ffecaaf6e345f457ca210e
Maxie <144727967+Maxie-M@users.noreply.github.com>
2024-09-24 11:56:17 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,12 @@
+//code.js
+//Maxie Machado 
+//Algorithms: Fibonacci Invariants 
+//Resources: Used two specific repositories for help: fibonacci-invariants-ClaytonBrown4741 and fibonacci-invariants-Countmooshroom
+// Plagiarism Statement: I certify that I have listed all sources used to complete this exercise, 
+// including the use of any Large Language Models. All of the work is my own, except where 
+// stated otherwise. I am aware that plagiarism carries severe penalties and that if plagiarism is
+// suspected, charges may be filed against me without prior notice.
+
 function fib(n) 
 {
     if (n==0) return [0];
@@ -11,3 +20,4 @@ function fib(n)
     }
     return arr;
 }
+
